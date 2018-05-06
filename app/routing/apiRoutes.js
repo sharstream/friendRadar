b54209aeb5dfc50b0872a12f8baa1eb2e85ece45
David Perez <david.perez@us.abb.com>
2018-05-06 16:14:19 -0400

    data friends in ajax request

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,24 +1,20 @@
 //api routes
-let surveys = require('../data/friends.js');
+let friends = require('../data/friends.js');
 let path = require('path');
 module.exports = (app) => {
 
-    app.get('/api/survey', function (req, res) {
-        res.json(surveys);
-    });
-
     app.get('/api/friends', function (req, res) {
-        res.json(surveys);
+        res.json(friends);
     });
 
-    app.post('/api/survey', function (req, res) {
-        surveys.push(req.body);
+    app.post('/api/friends', function (req, res) {
+        friends.push(req.body);
         res.json(true); // KEY LINE
     });
 
     app.post('/api/clear', function (req, res) {
         // Empty out the arrays of data
-        surveys = [];
-        console.log(surveys);
+        friends = [];
+        console.log(friends);
     })
 }
