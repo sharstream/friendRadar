7fa01ff43c266eb554cd81c96df2f2b38c1ea785
David Perez <david.perez@us.abb.com>
2018-05-06 12:56:32 -0400

    routes

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,22 @@
+//api routes
+let surveys = require('../data/friends.js');
+let path = require('path');
+module.exports = () => {
+    app.get('/api/survey', function (req, res) {
+        res.json(surveys);
+    });
+
+    app.post('/api/survey', function (req, res) {
+
+        surveys.push(req.body);
+        res.json(true); // KEY LINE
+
+    });
+
+    app.post('/api/clear', function (req, res) {
+        // Empty out the arrays of data
+        surveys = [];
+
+        console.log(surveys);
+    })
+}
