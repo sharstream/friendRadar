59317aa1cccbe93225d619def2d1c39652056d5d
David Perez <david.perez@us.abb.com>
2018-05-06 14:35:18 -0400

    both back and front end

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,22 +1,24 @@
 //api routes
 let surveys = require('../data/friends.js');
 let path = require('path');
-module.exports = () => {
+module.exports = (app) => {
+
     app.get('/api/survey', function (req, res) {
         res.json(surveys);
     });
 
-    app.post('/api/survey', function (req, res) {
+    app.get('/api/friends', function (req, res) {
+        res.json(surveys);
+    });
 
+    app.post('/api/survey', function (req, res) {
         surveys.push(req.body);
         res.json(true); // KEY LINE
-
     });
 
     app.post('/api/clear', function (req, res) {
         // Empty out the arrays of data
         surveys = [];
-
         console.log(surveys);
     })
 }
